aae6e06cafa4c4b3d1a9b7d07220a96481fa28d8
Edgar Ceron <edg-ceron@hotmail.com>
2018-01-07 00:03:14 -0600

    Esta actualizacion es todo lo del curso de introduccion a react

--- src/App.js
+++ src/App.js
@@ -1,52 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Title from './components/Title'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import Home from './pages/Home';
+import  './App.css';
 
 class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      numero: 0
-    }
-    this.updateNumero = this.updateNumero.bind(this)
+
+  constructor(props){
+    super(props);
   }
 
-  updateNumero () {
-    this.setState({
-      numero: this.state.numero + 1
-    })
-  }
-  render () {
+
+
+  render() {
     return (
-      <section>
-        <div>
-          <div>
-            <Title />
-            <h2>{this.state.numero}</h2>
-            <button onClick={this.updateNumero} >Crear cuenta gratuita</button>
-            <img src={process.env.PUBLIC_URL + '/imagenes/background.png'} />
-            <div>
-              <ul>
-                <li>
-                  <h3>Calificaciones con emociones</h3>
-                  <p>Califica tus lugares con experiencias, no con numeros</p>
-                </li>
-                <li>
-                  <h3>¿Sin internet? Sin problemas</h3>
-                  <p>Places funciona sin intenet o en conexiones lentas</p>
-                </li>
-                <li>
-                  <h3>Tus lugares favoritos</h3>
-                  <p>Define tu lista de sitios favoritos</p>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </section>
-    )
+      <MuiThemeProvider>
+
+        <Home></Home>
+
+      </MuiThemeProvider>
+    );
   }
 }
 
-export default App
+export default App;
